0d7d13ca09e6d019ef989c47111ebfa4b4fb3520
jonatanc04 <jcorcolesg98@gmail.com>
2024-01-08 17:16:24 +0100

    second

--- apptareas/src/App.js
+++ apptareas/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+//import ListaDeTareas from '../src/comps/ListaDeTareas';
+import titulo from './img/titulo.png';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app-tareas">
+      <div className="contenedor-titulo">
+        <img
+          src={titulo}
+          className="titulo"
+          alt="imagen-titlulo"
+        />
+      </div>
+      <div className="tareas-lista-principal">
+        <h1>Mis tareas</h1>
+         
+      </div>
     </div>
   );
 }
