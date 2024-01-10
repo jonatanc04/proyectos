d37adb93bb89eea3d894476e86e275c3c15e72c1
jonatanc04 <jcorcolesg98@gmail.com>
2024-01-10 16:14:08 +0100

    nuevo proyecto

---
+++ formhook/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import HookForm from './comps/HookForm';
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
---
+++ formhook/src/comps/HookForm.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import { useForm } from 'react-hook-form';
+
+function HookForm() {
+  return (
+
+  );
+}
+
+export default HookForm;
