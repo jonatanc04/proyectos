97befc9bfe5b1afbdd02ec09effe3ccb224c70f8
jonatanc04 <jcorcolesg98@gmail.com>
2024-01-30 11:11:06 +0100

    furule

--- pokeapi/src/App.js
+++ pokeapi/src/App.js
@@ -8,12 +8,26 @@ function App() {
   const [pokemons, setPokemon] = useState([]);
   const [search, setSearch] = useState('');
   const getData = async () => {
-    const res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=15&offset=0");
-    setPokemon(res.data.results);
+    const res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=1400&offset=0");
+    return res.data.results;
+  }
+
+  const getPokemonDetails = async (name) => {
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    return res.data;
+  }
+
+  const getPokemon = async () => {
+    const pokemonList = await getData();
+    const detailedPokemonList = await Promise.all(pokemonList.map(async (pokemon) => {
+      const details = await getPokemonDetails(pokemon.name);
+      return details;
+    }));
+    setPokemon(detailedPokemonList);
   }
 
   useEffect(() => {
-    getData()
+    getPokemon()
   },[])
 
   return (
--- pokeapi/src/comps/PokeRow.jsx
+++ pokeapi/src/comps/PokeRow.jsx
@@ -1,21 +1,16 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React from 'react'
 
 export const PokeRow = ({ pokemon }) => {
 
-  const [poke, setPokemon] = useState([]);
-  const getData = async () => {
-    const res = await axios.get(pokemon.url);
-    setPokemon(res.data);
-  }
-  getData();
-
   return (
-    <tr key={poke.id}>
-      <td>{poke.id}</td>
-      <td><img src={poke.sprites['front_default']} alt={poke.id}></img></td>
-      <td>{poke.name}</td>
-      <td>{poke.abilities[0].ability.name}</td>
+    <tr key={pokemon.id}>
+      <td>{pokemon.id}</td>
+      <td><img src={pokemon.sprites['front_default']} alt={pokemon.id}></img></td>
+      <td>{pokemon.name}</td>
+      <td>{pokemon.abilities[0].ability.name}</td>
+      <td>{pokemon.abilities[1] ? pokemon.abilities[1].ability.name : "not found"}</td>
+      <td>{pokemon.weight}</td>
+      <td>{pokemon.height}</td>
     </tr>
   )
 }
